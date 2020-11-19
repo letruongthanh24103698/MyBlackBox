83e4457a7f6b26c3681fdfd6f89068bb8800adf0
letruongthanh24103698 <letruongthanh3698@gmail.com>
2020-11-19 07:45:15 +0700

    Change File Name ok

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,9 @@
 #include "SD.h"
 #include "SPI.h"
 
+#include <WiFi.h>
+#include <HTTPClient.h>
+
 #define GPIOInit(_PIN_,_MODE_)              pinMode(_PIN_,_MODE_)
 #define GPIOWrite(_PIN_,_STATE_)            digitalWrite(_PIN_,_STATE_)
 #define GPIORead(_PIN_)                     digitalRead(_PIN_)
@@ -11,13 +14,24 @@
 
 #define _GPS_EN_PIN_              4 //GPIO4
 #define _SD_CS_PIN_               5 //GPIO5
-#define _GPS_PPS_PIN              2 //GPIO2
+#define _GPS_PPS_PIN              13 //GPIO13
+#define _ONBOARD_LED_PIN_         2
+
+#define _GPRMC_SPEED_ORDER_       7
+#define _GPRMC_TIME_ORDER_        1
+
+#define _KNOT_2_KMH_              1.852
 
 int32_t RxData_I32;
 SPIClass SPI_SD(VSPI);
-char* Path="/datalog_ESP32.txt";
-int64_t Size_I64=0;
-int64_t SentenceSize_I64=0;
+char* Path="/datalog_ESP32_default.txt";
+uint8_t Size_U8=0;
+uint8_t SentenceSize_U8=0;
+uint8_t Buffer_AU8[200];
+uint8_t SentenceBuffer_AU8[200];
+bool GetTimeDone_B=false;
+
+esp32web
 
 void writeFile(fs::FS &fs, const char * path, const char * message){
     // Serial.printf("Writing file: %s\n", path);
@@ -25,12 +39,15 @@ void writeFile(fs::FS &fs, const char * path, const char * message){
     File file = fs.open(path, FILE_WRITE);
     if(!file){
         Serial.println("Failed to open file for writing");
+        GPIOWrite(_ONBOARD_LED_PIN_,0);
         return;
     }
     if(file.print(message)){
         Serial.println("File written");
+        GPIOWrite(_ONBOARD_LED_PIN_,1);
     } else {
         Serial.println("Write failed");
+        GPIOWrite(_ONBOARD_LED_PIN_,0);
     }
     file.close();
 }
@@ -41,12 +58,15 @@ void appendFile(fs::FS &fs, const char * path, const char * message){
     File file = fs.open(path, FILE_APPEND);
     if(!file){
         Serial.println("Failed to open file for appending");
+        GPIOWrite(_ONBOARD_LED_PIN_,0);
         return;
     }
     if(file.print(message)){
         Serial.println("Message appended");
+        GPIOWrite(_ONBOARD_LED_PIN_,1);
     } else {
         Serial.println("Append failed");
+        GPIOWrite(_ONBOARD_LED_PIN_,0);
     }
     file.close();
 }
@@ -57,9 +77,11 @@ void readFile(fs::FS &fs, const char * path, uint8_t *buffer_AU8, uint64_t *size
   File file = fs.open(path);
   if(!file){
       Serial.println("Failed to open file for reading");
+      GPIOWrite(_ONBOARD_LED_PIN_,0);
       return;
   }
 
+  GPIOWrite(_ONBOARD_LED_PIN_,1);
   Serial.print("Read from file: ");\
   
   *size=0;
@@ -69,12 +91,104 @@ void readFile(fs::FS &fs, const char * path, uint8_t *buffer_AU8, uint64_t *size
   file.close();
 }
 
+int16_t GetCharPos(uint8_t *buf, uint8_t len, uint8_t ch)
+{
+  for (uint8_t i=0; i<len; i++)
+  {
+    if (buf[i]==ch)
+      return i;
+  }
+  return -1;
+}
+
+void New_Speed_Handle(float speed)
+{
+  Serial.print("Speed :");
+  Serial.println(speed);
+  //TODO HANDLE SPEED CHANGE
+}
+
+void RenameFile(char *buf)
+{
+  char *newname="";
+  sprintf(newname,"/datalog_ESP32_%s.txt",buf);
+  SD.rename(Path, newname);
+  memcpy(Path,newname, strlen(buf));
+}
+
+void Decode_Sentence()
+{
+  uint8_t *buf=SentenceBuffer_AU8;
+  uint8_t len=SentenceSize_U8;
+  buf[len]=0;
+  appendFile(SD, Path, (char *)buf);
+  int16_t pos=0;
+  int16_t nextpos=0;
+  float speed=-1;
+  if (len<=6)
+    return;
+  if (memcmp(buf,"$GPRMC",6)==0)
+  {
+    for (uint8_t i=0; i<_GPRMC_SPEED_ORDER_; i++)
+    {
+      pos=GetCharPos(buf,len,',')+1;
+      if (pos!=-1)
+      {
+        buf=buf+pos;
+        len=len-pos-1;
+      }
+      else
+        break;
+    }
+
+    nextpos=GetCharPos(buf,len,',');
+    if (nextpos!=-1)
+    {
+      buf[nextpos]=0;
+      speed=atof((char *)buf)*_KNOT_2_KMH_;
+      New_Speed_Handle(speed);
+    }
+
+    for (uint8_t i=0; i<_GPRMC_TIME_ORDER_; i++)
+    {
+      pos=GetCharPos(buf,len,',')+1;
+      if (pos!=-1)
+      {
+        buf=buf+pos;
+        len=len-pos-1;
+      }
+      else
+        break;
+    }
+
+    nextpos=GetCharPos(buf,len,',');
+    if (nextpos!=-1 && (nextpos-pos)>0)
+    {
+      buf[nextpos]=0;
+      float tmp=atof((char*) buf)+70000.0;
+      char tmp_c[20];
+      sprintf(tmp_c,"%f",tmp);
+      RenameFile((char *)buf);
+      GetTimeDone_B=true;
+    }
+  }
+}
+
 void GPS_Receive_Handle(uint8_t ch)
 {
-  if (ch=='\r' || ch=='\n')
+  if (ch=='\n')
   {
-
+    SentenceSize_U8 = Size_U8;
+    memcpy(SentenceBuffer_AU8, Buffer_AU8, SentenceSize_U8);
+    Decode_Sentence();
+    SentenceSize_U8=0;
+    Size_U8=0;
   }
+  else
+  {
+    Buffer_AU8[Size_U8++]=ch;
+  }
+  
 }
 
 void PPS_INT_Handle()
@@ -84,14 +198,15 @@ void PPS_INT_Handle()
     RxData_I32=Serial2.read();
     if (RxData_I32!=-1)
     {
-
+      Serial.print((char)RxData_I32);
+      GPS_Receive_Handle(RxData_I32);
     }
     else
     {
       break;
     }
     
-  } while (-1);
+  } while (1);
   
 }
 
@@ -105,8 +220,8 @@ void setup() {
       Serial.println("Card Mount Failed");
       return;
   }
-  /*writeFile(SD, Path, "aaa");
-  uint8_t temp[4];
+  writeFile(SD, Path, "");
+  /*uint8_t temp[4];
   uint64_t size;
   readFile(SD, Path ,temp, &size);
   temp[3]=0;
@@ -116,12 +231,17 @@ void setup() {
   GPIOInit(_GPS_EN_PIN_,OUTPUT);
   GPIOWrite(_GPS_EN_PIN_,1);
 
+  GPIOInit(_ONBOARD_LED_PIN_, OUTPUT);
+  GPIOWrite(_ONBOARD_LED_PIN_,1);
+
   GPIOInit(_GPS_PPS_PIN,INPUT_PULLDOWN);
   GPIOAttachINT(_GPS_PPS_PIN,PPS_INT_Handle,FALLING);
 
   Serial.println("Start...");
+  GPIOWrite(_ONBOARD_LED_PIN_,1);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  PPS_INT_Handle();
 }
